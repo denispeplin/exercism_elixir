4ab1cf16346e919e72c623548d9783a6f4db8bfc
denispeplin <denis.peplin@gmail.com>
2016-06-24 22:05:26 +0300

    Implement 'bob' exercise.

--- bob/bob.exs
+++ bob/bob.exs
@@ -1,8 +1,18 @@
 defmodule Bob do
+  @doc ~S"""
+  http://www.pcre.org/pcre.txt
+  \p{Lu} always matches an upper case letter
+  """
   def hey(input) do
     cond do
-        true -> raise "Your implementation goes here"
-
+      String.ends_with?(input, "?") ->
+        "Sure."
+      Regex.match?(~r/^[ ]{0,}$/, input) ->
+        "Fine. Be that way!"
+      Regex.match?(~r/\p{Lu}{2}/u, input) ->
+        "Whoa, chill out!"
+      true ->
+        "Whatever."
     end
   end
 end
