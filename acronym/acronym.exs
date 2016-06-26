6677f253e80e6a5534431f221674f5c5b717989f
denispeplin <denis.peplin@gmail.com>
2016-06-26 13:22:24 +0300

    Upcase letters after spaces.

--- acronym/acronym.exs
+++ acronym/acronym.exs
@@ -5,13 +5,17 @@ defmodule Acronym do
   """
   @spec abbreviate(String.t()) :: String.t()
   def abbreviate(string) do
-    _abbreviate(string, [])
+    _abbreviate(string, [], true)
   end
-  defp _abbreviate("", acc) do
+  defp _abbreviate("", acc, _) do
     Enum.join acc
   end
-  defp _abbreviate(string, acc) do
+  defp _abbreviate(string, acc, false) do
     { grapheme, reminder } = String.next_grapheme string
-    _abbreviate reminder, acc ++ Regex.scan(~r/^\p{Lu}$/u, grapheme)
+    _abbreviate reminder, acc ++ Regex.scan(~r/^\p{Lu}$/u, grapheme), grapheme == " "
+  end
+  defp _abbreviate(string, acc, true) do
+    { grapheme, reminder } = String.next_grapheme string
+    _abbreviate reminder, acc ++ [String.upcase(grapheme)], false
   end
 end
