134a7b139f358c81e8fb01eb5e2bce3afd51c8f1
denispeplin <denis.peplin@gmail.com>
2018-11-06 21:17:54 +0400

    Implement Strain.

---
+++ strain/strain.exs
@@ -0,0 +1,37 @@
+defmodule Strain do
+  @doc """
+  Given a `list` of items and a function `fun`, return the list of items where
+  `fun` returns true.
+
+  Do not use `Enum.filter`.
+  """
+  @spec keep(list :: list(any), fun :: (any -> boolean)) :: list(any)
+  def keep(list, fun, acc \\ [])
+  def keep([], _fun, acc), do: Enum.reverse(acc)
+
+  def keep([elem | list], fun, acc) do
+    if fun.(elem) do
+      keep(list, fun, [elem | acc])
+    else
+      keep(list, fun, acc)
+    end
+  end
+
+  @doc """
+  Given a `list` of items and a function `fun`, return the list of items where
+  `fun` returns false.
+
+  Do not use `Enum.reject`.
+  """
+  @spec discard(list :: list(any), fun :: (any -> boolean)) :: list(any)
+  def discard(list, fun, acc \\ [])
+  def discard([], _fun, acc), do: Enum.reverse(acc)
+
+  def discard([elem | list], fun, acc) do
+    if fun.(elem) do
+      discard(list, fun, acc)
+    else
+      discard(list, fun, [elem | acc])
+    end
+  end
+end
